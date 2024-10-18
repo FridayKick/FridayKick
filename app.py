9cc69b06a56c38e51a2404fc5ff29a42c8782337
FridayKick <h.nasser@windowslive.com>
2024-10-18 11:18:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.secret_key = 'supersecretkey'
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # MySQL-Verbindung mit SSL-Zertifikat
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://fridaykickadmin:Passw0rd!1989@fridaykicksql.mysql.database.azure.com/fridaykickdb?ssl_disabled=true&ssl_ca='
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://fridaykickadmin:Passw0rd!1989@fridaykicksql.mysql.database.azure.com/fridaykickdb?ssl_ca=/home/site/wwwroot/BaltimoreCyberTrustRoot.crt.pem'
 
 
 # SSL-Zertifikatskonfiguration für die Datenbankverbindung
